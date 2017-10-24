ab049effca195664781ac3a6ed8954609b53f699
Gurkan Oluc <gurkanoluc@gmail.com>
2017-10-24 12:30:58 +0100

    Make tests Go 1.6 compatible

--- newrelic_test.go
+++ newrelic_test.go
@@ -5,28 +5,20 @@ import (
 )
 
 func TestBuildNewRelicConfig(t *testing.T) {
-	t.Run("When Newrelic tracing is disabled", func(t *testing.T) {
-		config := BuildNewRelicConfig("false", "foo")
+	cases := []struct {
+		enabled, licenseKey string
+		expected            bool
+	}{
+		{"false", "roo", false},
+		{"", "foo", true},
+		{"", "", false},
+	}
 
-		if config.Enabled != false {
-			t.Error("expected", false, "got", config.Enabled)
+	for _, c := range cases {
+		config := BuildNewRelicConfig(c.enabled, c.licenseKey)
+
+		if config.Enabled != c.expected {
+			t.Error("expected", c.expected, "got", config.Enabled)
 		}
-	})
-
-	t.Run("When Newrelic tracing is enabled", func(t *testing.T) {
-		config := BuildNewRelicConfig("", "foo")
-
-		if config.Enabled != true {
-			t.Error("expected", true, "got", config.Enabled)
-		}
-	})
-
-	t.Run("When Newrelic license key is not set", func(t *testing.T) {
-		config := BuildNewRelicConfig("", "")
-
-		if config.Enabled != false {
-			t.Error("expected", false, "got", config.Enabled)
-		}
-	})
-
+	}
 }
